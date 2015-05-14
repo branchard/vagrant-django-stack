aa55ceff25e417f1ee3b4d899549b2ffd9888088
Benoit <benoit.branchard@gmail.com>
2015-05-14 16:01:33 +0200

    gunicorn_run.sh permission fix

--- gunicorn_run.sh
+++ gunicorn_run.sh
@@ -5,14 +5,14 @@ set -e
 LOGFILE=/var/log/gunicorn/mlorant.log
 LOGDIR=$(dirname $LOGFILE)
 LOGLEVEL=debug   # debug, info or warning
-NUM_CORES=cat /proc/cpuinfo | grep processor | wc -l
+NUM_CORES=sudo cat /proc/cpuinfo | grep processor | wc -l
 let "NUM_WORKERS=2*$NUM_CORES+1"
 
 # user/group to run as
 USER=root
 GROUP=root
 
-cd /vagrant/
+cd /vagrant/toto/
 test -d $LOGDIR || mkdir -p $LOGDIR
 exec gunicorn_django -w $NUM_WORKERS \
   --user=$USER --group=$GROUP --log-level=$LOGLEVEL \
