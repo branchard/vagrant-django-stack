b4d15db2dfc8ef0936f0a2a4cfa4416a8b8056a5
DigitalSuricate <benoit.branchard@gmail.com>
2015-05-18 16:26:43 +0200

    MySQL Driver fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -39,28 +39,29 @@ debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password
 debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password $MYSQL_PASSWORD"
 apt-get -y install mysql-server
 apt-get -y install mysql-client
+apt-get -y install libmysqlclient-dev
 echo 'Done.'
 
 # Python + virtualenv
 echo "A Python$PYTHON_VERSION will be initialized with a virtualenv"
-apt-get -y install python python$PYTHON_VERSION python-pip
+apt-get -y install python python$PYTHON_VERSION python$PYTHON_VERSION-dev python-pip
 
 pip install virtualenv
 pip install virtualenvwrapper
 
 echo '' >> $HOME_DIR.bashrc
 echo '# CUSTOM' >> $HOME_DIR.bashrc
-echo "export WORKON_HOME=~/.virtualenvs" >> $HOME_DIR.bashrc
+echo "export WORKON_HOME=$PROJECTS_DIR/virtualenvs" >> $HOME_DIR.bashrc
 echo 'mkdir -p $WORKON_HOME' >> $HOME_DIR.bashrc
-echo "export PROJECT_HOME=$PROJECTS_DIR" >> $HOME_DIR.bashrc
+echo "export PROJECT_HOME=$PROJECTS_DIR/$PROJECT_NAME" >> $HOME_DIR.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> $HOME_DIR.bashrc
 echo "workon $VIRTUALENV_NAME" >> $HOME_DIR.bashrc
 echo "echo 'virtualenv acivated'" >> $HOME_DIR.bashrc
-#echo 'cd $PROJECT_HOME' >> $HOME_DIR.bashrc
+echo 'cd $PROJECT_HOME' >> $HOME_DIR.bashrc
 
-export WORKON_HOME=$HOME_DIR/.virtualenvs
+export WORKON_HOME=$PROJECTS_DIR/virtualenvs
 mkdir -p $WORKON_HOME
-export PROJECT_HOME="$PROJECTS_DIR"
+export PROJECT_HOME="$PROJECTS_DIR/$PROJECT_NAME"
 source /usr/local/bin/virtualenvwrapper.sh
 
 echo 'done.'
@@ -68,11 +69,9 @@ echo 'done.'
 echo 'Virtualenv activation ...'
 mkvirtualenv --python=/usr/bin/python$PYTHON_VERSION $VIRTUALENV_NAME
 echo 'Virtualenv activating now'
-echo 'We are now in the virtualenv !'
 
-# Python dependences
-
-pip install --allow-all-external mysql-connector-python
+# Python dependencies
+pip install git+git://github.com/davispuh/MySQL-for-Python-3
 
 # Django installation
 echo "Django $DJANGO_VERSION will be installed"
@@ -83,10 +82,18 @@ echo 'Done.'
 if [ ! -f $PROJECTS_DIR/$PROJECT_NAME/manage.py ];
   then
     echo "The project $PROJECT_NAME don\'t exist, a new project will be init"
-    (cd $PROJECTS_DIR && django-admin startproject $PROJECT_NAME)
+    (cd $PROJECTS_DIR && django-admin startproject $PROJECT_NAME && \
+     cd $PROJECT_NAME && pip freeze > requirements.txt)
     echo 'Done.'
   else
     echo "The project $PROJECT_NAME exist"
+    echo "Checking existing dependencies ..."
+    if [ ! -f $PROJECTS_DIR/$PROJECT_NAME/requirements.txt ];
+      then
+      echo "exiting requirements"
+      echo "requirements installation ..."
+      pip install -r $PROJECTS_DIR/$PROJECT_NAME/requirements.txt
+    fi
     echo 'Done.'
 fi
 
